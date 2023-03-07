764f063fdab407353687d6f40c806a2d2212e469
david-eticos <jsaavedra@eticos.com>
2023-03-07 00:46:26 -0500

    version logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
-
-import React from 'react'
-import { Switch, Route} from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { login } from './components/login';
-import { actaSumi } from './components/actasSuministro';
-import { actaSatel } from './components/actasSatelites';
+import { login } from "./components/login";
+import { actaSumi } from "./components/actasSuministro";
+import { actaSatel } from "./components/actasSatelites";
 
 function App() {
   return (
-
     // <Router>
-      <div>
-  
-        <Switch>
-          <Route exact path='/' component={login}/>
-          <Route exact path='/actas/suministro' component={actaSumi}/>
-          <Route exact path='/actas/satelite' component={actaSatel}/>
-        </Switch>
-
-      </div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={login} />
+        <Route exact path="/actas/suministro" component={actaSumi} />
+        <Route exact path="/actas/satelite" component={actaSatel} />
+      </Switch>
+    </div>
 
     // </Router>
-
-
   );
 }
 
